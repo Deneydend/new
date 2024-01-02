40265f36e5bfea83e9b2c59c94a6ecf07ade46fd
Deneydend <124338744+Deneydend@users.noreply.github.com>
2024-01-02 09:29:24 +0300

    Update mag3.js 02.01.24 Price

--- mag3.js
+++ mag3.js
@@ -7,9 +7,9 @@ var ektrabic = document.getElementById("ektrabic"); // EKSTRA BIÇAK
 
 
 // SUNUCU VERİLERİ 
-let SncHzr = 11;
-let SncGrf = 12;
-let SncTm = 18;
+let SncHzr = 15;
+let SncGrf = 16;
+let SncTm = 25;
 let SncExt = 25;
 
 
